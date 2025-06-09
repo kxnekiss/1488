61f5adc9d83b2d8fa528fde0b398adfe3115903c
kxnekiss <tsobin2005stepan@yandex.ru>
2025-06-10 02:42:26 +0700

    huita

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,42 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import User, Client, Car, Route, Order, Payment, Delivery
 
-from .models import User, Room, Booking
 
-
-class UserAdmin(BaseUserAdmin):
-    fieldsets = BaseUserAdmin.fieldsets + (
-        (None, {'fields': ('failed_attempts', 'first_login')}),
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    model = User
+    list_display = ('username', 'email', 'is_active', 'is_staff', 'first_login', 'failed_attempts')
+    fieldsets = DefaultUserAdmin.fieldsets + (
+        ('Дополнительно', {'fields': ('first_login', 'failed_attempts')}),
     )
 
 
-admin.site.register(User, UserAdmin)
-admin.site.register(Room)
-admin.site.register(Booking)
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('full_name',)
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('license_plate', 'brand', 'capacity_kg')
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ('origin', 'destination')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'route', 'delivery_date', 'car')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'payment_date', 'amount')
+
+
+@admin.register(Delivery)
+class DeliveryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'weight_kg')
